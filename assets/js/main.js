3f389fe0c776a42590da07b03b002058407f5854
TiniQ <83157726+TiniQ@users.noreply.github.com>
2021-04-26 19:17:22 +0600

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(($) => {
-    $('.swipe__item').on('click', function() {
-        $('.swipe__item.active').removeClass('active')
-            .find('.swipe__info').slideToggle(300);
-        $(this).toggleClass('active')
-            .find('.swipe__info').slideToggle(300);
-    });
-})
